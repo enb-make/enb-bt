1bbf9902fbb197ee10234dd5763e8d962794a287
Marat Dulin <mdevils@yandex.ru>
2013-11-18 22:04:40 +0400

    Version 0.0.17

--- techs/bt-server.js
+++ techs/bt-server.js
@@ -40,11 +40,32 @@ module.exports = require('enb/lib/build-flow').create()
         function buildRequire(absPath, pre, post) {
             var relPath = node.relativePath(absPath);
             return [
-                'delete require.cache[require.resolve("' + relPath + '")];',
+                'dropRequireCache(require, require.resolve("' + relPath + '"));',
                 (pre || '') + 'require("' + relPath + '")' + (post || '') + ';'
             ].join('\n');
         }
+
+        var dropRequireCacheFunc = [
+            'function dropRequireCache(requireFunc, filename) {',
+            '    var module = requireFunc.cache[filename];',
+            '    if (module) {',
+            '        if (module.parent) {',
+            '            if (module.parent.children) {',
+            '                var moduleIndex = module.parent.children.indexOf(module);',
+            '                if (moduleIndex !== -1) {',
+            '                    module.parent.children.splice(moduleIndex, 1);',
+            '                }',
+            '            }',
+            '            delete module.parent;',
+            '        }',
+            '        delete module.children;',
+            '        delete requireFunc.cache[filename];',
+            '    }',
+            '};'
+        ].join('\n');
+
         return [
+            dropRequireCacheFunc,
             buildRequire(this._btFile, 'var BT = '),
             'var bt = new BT();',
             btFiles.map(function(file) {
