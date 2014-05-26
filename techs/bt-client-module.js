4fa7893939040fc31c767f9f5b801c0a2d4b40d8
Marat Dulin <mdevils@yandex.ru>
2014-05-26 14:11:24 +0400

    Version 1.0.0

--- techs/bt-client-module.js
+++ techs/bt-client-module.js
@@ -1,18 +1,19 @@
 /**
  * bt-client-module
  * ================
- * 
- * Склеивает *bt*-файлы по deps'ам с помощью набора `require` в виде `?.bt.client.js`.
+ *
+ * Склеивает *bt*-файлы по deps'ам в виде `?.bt.client.js`.
  * Предназначен для сборки клиентского BT-кода.
  * Использует модульную обертку.
- * 
+ *
  * **Опции**
- * 
+ *
  * * *String* **target** — Результирующий таргет. По умолчанию — `?.bt.client.js`.
- * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
- * 
+ * * *String* **filesTarget** — files-таргет, на основе которого получается список
+ *   исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
+ *
  * **Пример**
- * 
+ *
  * ```javascript
  * nodeConfig.addTech(require('bt/techs/bt-client-module'));
  * ```
@@ -38,22 +39,25 @@ module.exports = require('enb/lib/build-flow').create()
     .builder(function(btFiles) {
         var node = this.node;
         var dependencies = this._dependencies;
+        var btFilePath = this._btFile;
+        var targetPath = node.resolvePath(this._target);
         return Vow.all([
-            vowFs.read(this._btFile, 'utf8').then(function(data) {
-                return data;
+            vowFs.read(btFilePath, 'utf8').then(function(content) {
+                return {
+                    filename: node.relativePath(btFilePath),
+                    content: content
+                };
             }),
             Vow.all(btFiles.map(function(file) {
-                return vowFs.read(file.fullname, 'utf8').then(function(data) {
-                    var relPath = node.relativePath(file.fullname);
-                    return '// begin: ' + relPath + '\n' +
-                        btClientProcessor.process(data) + '\n' +
-                        '// end: ' + relPath + '\n';
+                return vowFs.read(file.fullname, 'utf8').then(function(content) {
+                    return {
+                        filename: node.relativePath(file.fullname),
+                        content: btClientProcessor.process(content)
+                    };
                 });
-            })).then(function(sr) {
-                return sr.join('\n');
-            })
-        ]).spread(function(btEngineSource, inputSources) {
-            return btClientProcessor.buildModule(btEngineSource, inputSources, dependencies);
+            }))
+        ]).spread(function(btEngine, inputSources) {
+            return btClientProcessor.buildModule(targetPath, btEngine, inputSources, dependencies);
         });
     })
     .createTech();
