860809821f4715175fcd52a5b149b7160802f83d
Andrey Morozov <alt-j@yandex-team.ru>
2015-12-10 15:46:12 +0300

    Добавлена возможность фризить ссылки на ресурсы внутри шаблонов.

--- techs/bt-client-module.js
+++ techs/bt-client-module.js
@@ -12,6 +12,7 @@
  * * *Object* **dependencies** — Зависимости. По умолчанию — `{i18n: 'y-i18n'}`.
  * * *String* **filesTarget** — files-таргет, на основе которого получается список
  *   исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
+ * * *Boolean* **freezeLinks** — Нужно ли замораживать ссылки внтри файлов. По умолчанию — `false`.
  *
  * **Пример**
  *
@@ -19,9 +20,11 @@
  * nodeConfig.addTech(require('enb-bt/techs/bt-client-module'));
  * ```
  */
- var vow = require('vow'),
-    vowFs = require('enb/lib/fs/async-fs'),
-    btClientProcessor = require('../lib/bt-client-processor');
+
+var vow = require('vow');
+var vowFs = require('enb/lib/fs/async-fs');
+var btClientProcessor = require('../lib/bt-client-processor');
+var BorschikPreprocessor = require('enb-borschik/lib/borschik-preprocessor');
 
 module.exports = require('enb/lib/build-flow').create()
     .name('bt-client-module')
@@ -29,6 +32,7 @@ module.exports = require('enb/lib/build-flow').create()
     .defineOption('useSourceMap', true)
     .defineOption('btFile', '')
     .defineOption('dependencies', {i18n: 'y-i18n'})
+    .defineOption('freezeLinks', false)
     .useFileList(['bt.js'])
     .needRebuild(function(cache) {
         this._btFile = this._btFile || 'node_modules/enb-bt/lib/bt.js';
@@ -40,9 +44,9 @@ module.exports = require('enb/lib/build-flow').create()
     })
     .builder(function(btFiles) {
         var node = this.node;
-        var dependencies = this._dependencies;
+
         var btFilePath = this._btFile;
-        var targetPath = node.resolvePath(this._target);
+        var freezeLinks = this._freezeLinks;
         return vow.all([
             vowFs.read(btFilePath, 'utf8').then(function(content) {
                 return {
@@ -50,16 +54,40 @@ module.exports = require('enb/lib/build-flow').create()
                     content: content
                 };
             }),
-            vow.all(btFiles.map(function(file) {
-                return vowFs.read(file.fullname, 'utf8').then(function(content) {
-                    return {
-                        filename: node.relativePath(file.fullname),
-                        content: btClientProcessor.process(content)
-                    };
+            vow.all(btFiles.map(function(file, index) {
+                if (!freezeLinks) {
+                    return vowFs.read(file.fullname, 'utf8').then(function (content) {
+                        return {
+                            filename: node.relativePath(file.fullname),
+                            content: btClientProcessor.process(content)
+                        };
+                    });
+                }
+
+                return node.createTmpFileForTarget('#' + index).then(function (tmpFile) {
+                    return new BorschikPreprocessor()
+                        .preprocessFile(file.fullname, tmpFile, true, false, false)
+                        .then(function () {
+                            return vowFs.read(tmpFile, 'utf8');
+                        })
+                        .then(function (content) {
+                            return vowFs.remove(tmpFile).then(function () {
+                                return {
+                                    filename: node.relativePath(file.fullname),
+                                    content: btClientProcessor.process(content)
+                                };
+                            });
+                        });
                 });
             }))
         ]).spread(function(btEngine, inputSources) {
-            return btClientProcessor.buildModule(targetPath, btEngine, inputSources, dependencies, this._useSourceMap);
+            return btClientProcessor.buildModule(
+                node.resolvePath(this._target),
+                btEngine,
+                inputSources,
+                this._dependencies,
+                this._useSourceMap
+            );
         }.bind(this));
     })
     .createTech();
